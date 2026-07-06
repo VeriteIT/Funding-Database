bce19d57ec5a74f248c6c624cd42ea537329eb10
Verite IT <66620291+VeriteIT@users.noreply.github.com>
2026-07-07 00:39:59 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Set base to the repo/subpath name when deploying to GitHub Pages.
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE || "/",
-});
